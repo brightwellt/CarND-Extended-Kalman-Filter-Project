f96b843f810f38ebaeffe2411bdae7e365c2b5f5
brightwellt <brightwellt@users.noreply.github.com>
2017-07-22 11:55:14 +0100

    Adding source code changes to EKF project
    Code altered via notepad++ and Docker environment

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,6 +36,21 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+  H_laser_ << 1, 0, 0, 0, // Lesson 5, 10
+              0, 1, 0, 0;
+
+			  
+  //ekf_.F_ = // 4x4 matrix (state transition) - done in ProcessMeasurement
+  // ekf_.P_ = // 4x4 matrix // https://discussions.udacity.com/t/rmse-for-x-starts-high-and-does-not-dip-to-0-11-by-the-end-of-the-data-feed/309561
+  ekf_.P_ = MatrixXd(4,4);
+  ekf_.P_ << 1, 0, 0, 0,
+          0, 1, 0, 0,
+          0, 0, 100, 0,
+          0, 0, 0, 100;
+			  
+  //set the acceleration noise components
+  //noise_ax = 9; // lesson 5 section 13
+  //noise_ay = 9;
 
 
 }
@@ -61,19 +76,37 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    ekf_.x_ << 1, 1, 1, 1; // this value is important for the RMSE - play with the latter two values as we may accumulate high RMSE at the beginning
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
+	  We get rho, theta, rho_dot. Convert them over!
       */
+	  //Just set ekf_.x_{0} to ro*cos(theta)
+	  //Just set ekf_.x_{1} to ro*sin(theta)
+	  float ro = measurement_pack.raw_measurements_[0];
+	  float theta = measurement_pack.raw_measurements_[1];
+	  ekf_.x_ << ro*cos(theta), ro*sin(theta), 0, 0;
+	  
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+	  // Copy x and y over.
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+      
     }
 
+	//ekf_.F_ << set to 1 diagonal matrix (4x4, following classroom stuff)
+	ekf_.F_ = MatrixXd(4, 4);
+	ekf_.F_ << 1, 0, 0, 0,
+	           0, 1, 0, 0,
+			   0, 0, 1, 0,
+			   0, 0, 0, 1;
+	previous_timestamp_ = measurement_pack.timestamp_;
+	
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
@@ -90,6 +123,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+   //compute the time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  //Modify the F matrix so that the time is integrated lesson 5.8
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  float noise_ax = 9; // lesson 5 section 13
+  float noise_ay = 9;
+  //set the process covariance matrix Q lesson 5.9
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+         0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+         dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+         0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
 
   ekf_.Predict();
 
@@ -105,8 +158,20 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+	// Set ekf_.H_ by setting to Hj, the calculated jacobian 
+	// set ekf_.R_ by just using R_radar_
+	Hj_ = tools.CalculateJacobian(ekf_.x_);
+	ekf_.H_= Hj_;
+	ekf_.R_ = R_radar_;
+	
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+	// Set ekf_.H_ by just using H_laser_
+	// Set ekf_.R by just using R_laser
+	ekf_.H_ = H_laser_;
+	ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,18 +22,67 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  // lidar and radar use the same function
+  x_ = F_ * x_; // lesson 5, 8
+  MatrixXd Ft = F_.transpose(); // lesson 5, 9
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Kalman Filter equations
+	lesson 5, 7
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
-    * update the state by using Extended Kalman Filter equations
+    * update the state by using Extended Kalman Filter equations 5.14
   */
+  float x1 = x_(0);
+  float y1 = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  
+  // Avoid zero here
+  float rho = sqrt(x1 * x1 + y1 * y1);
+  float theta = 0.0;
+  if (fabs(x1) > 0.0001) {
+	  theta = atan2(y1, x1);
+  }
+  float ro_dot = 0.0;
+  if (fabs(rho) > 0.0001) {
+	  ro_dot = (x1 * vx + y1 * vy)/rho;
+  }
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho,theta, ro_dot;
+  
+  VectorXd y = z - z_pred;
+  y[1] = atan2(sin(y[1]), cos(y[1]));
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
 }
